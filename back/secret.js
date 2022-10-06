ca038d04cd3ad7100bdbe266bd5d85984e0e6b81
jiho <jenny.lee7316@gmail.com>
2022-10-06 10:11:23 +0900

    secret.js 파일 추가

---
+++ back/secret.js
@@ -0,0 +1,11 @@
+// 문자열 아무거나 괜찮다
+exports.jwtSecret = "jwt-secret-8520";
+
+exports.databaseSecret = {
+    // host는 ip를 말하는 것으로 도메인 넣어주면된다
+    host: "www.seongongus.site",
+    user: "outer",
+    port: "3306",
+    password: "111111",
+    database: "MyTodoDB",
+}
